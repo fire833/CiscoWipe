76c1782e29a7c061baa703851b37f575c1126514
fire833 <kttpsy@gmail.com>
2021-02-23 23:28:38 -0600

    Started rewrite
    Started to implement pure serial communication for the application. Began to create the keyword search that runs and triggers different things at different key phrases, indicating which device is being wiped, what phase it is in, etc. Also added some commands to the commands text lists for each device.

--- Dir/ciscowipe.py
+++ Dir/ciscowipe.py
@@ -1,16 +1,21 @@
 
 #import pyautogui
-import keyboard
+# import keyboard
 import os
 import sys
 import time
 import argparse
-import cmd
 import serial
 import selenium
+import nltk
+import pynput
+import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from pynput.keyboard import Key, Controller, Listener, KeyCode
+from pynput import keyboard
+
 
 # Random Vars
 
@@ -59,126 +64,250 @@ chromedriver = os.path.join(sys.path[0], 'chromedriver.exe')
 with open(wap_commands_list, 'r') as wap:
     cm1 = wap.read()
     wlist = cm1.splitlines()
-    wlen = len(wlist)
+    wlen = str(len(wlist))
 
 with open(switch_commands_list, 'r') as switch:
     cm2 = switch.read()
     slist = cm2.splitlines()
-    slen = len(slist)
+    slen = str(len(slist))
 
 with open(router_commands_list, 'r') as router:
     cm3 = router.read()
     rlist = cm3.splitlines()
-    rlen = len(rlist)
+    rlen = str(len(rlist))
 
 with open(asa_commands_list, 'r') as asa:
     cm4 = asa.read()
     alist = cm4.splitlines()
-    alen = len(alist)
+    alen = str(len(alist))
 
 # Basic purge all files based on command list functions
 
-def wap_file_del():
-    keyboard.write(enter)
-    for x in wlist:
-        keyboard.write(rm1 + x + 3 * enter)
-        time.sleep(sleep_time)
-    keyboard.write('delete /recursive /force flash:configs\n')
-    keyboard.write('# Succesfully Removed/Attempted to Remove ')
-    keyboard.send(wlen)
-    keyboard.write(' files from WAP!\n# Here is a list of remaining files: \n' + dirflash + enter)
+class main_del():
 
-def switch_file_del():
-    keyboard.write(enter)
-    for y in slist:
-        keyboard.write(rm + y + enter + 'y' + enter)
-        time.sleep(sleep_time)
-    keyboard.write('# Succesfully Removed/Attempted to Remove ')
-    keyboard.send(slen)
-    keyboard.write(' files from Switch!\n# Here is a list of remaining files: \n' + dirflash + enter)
+    import time
+    
+    def write(self, data):
+        keyboard = Controller()
+        keyboard.type(data)
 
-def router_file_del():
-    keyboard.write(enter)
-    for z in rlist:
-        keyboard.write(rm + z + 3 * enter)
-        time.sleep(sleep_time)
-    keyboard.write('# Succesfully Removed/Attempted to Remove ')
-    keyboard.send(rlen)
-    keyboard.write(' files from Router!\n# Here is a list of remaining files: \n' + dirflash + enter)
+    def wap_file_del(self):
+    
+        # WAP Deletion logic
+        self.write(enter)
+        for x in wlist:
+            self.write(rm1 + x + 3 * enter)
+            time.sleep(sleep_time)
+        self.write('delete /recursive /force flash:configs\n')
+        self.write('# Succesfully Removed/Attempted to Remove ' + wlen + ' files from WAP!\n# Here is a list of remaining files: \n' + dirflash)
 
-def asa_file_del():
-    keyboard.write(enter)
-    for w in alist:
-        keyboard.write(rm1 + w + 4 * enter)
-        time.sleep(sleep_time)
-    keyboard.write('# Succesfully Removed/Attempted to Remove ')
-    keyboard.send(alen)
-    keyboard.write(' files from ASA!\n# Here is a list of remaining files: \n' + dirflash + enter)
+    def switch_file_del(self):
+    
+        # Switch Deletion Logic
+        self.write(enter)
+        for y in slist:
+            self.write(rm + y + 3 * enter)
+            time.sleep(sleep_time)
+        self.write('# Succesfully Removed/Attempted to Remove ' + slen + ' files from Switch!\n# Here is a list of remaining files: \n' + dirflash)
 
-# ROMMON RESET Functions
+    def router_file_del(self):
+    
+        # Router Deletion Logic
+        self.write(enter)
+        for z in rlist:
+            self.write(rm + z + 3 * enter)
+            time.sleep(sleep_time)
+        self.write('# Succesfully Removed/Attempted to Remove ' + rlen + ' files from Router!\n# Here is a list of remaining files: \n' + dirflash)
 
-def router_rommon_exec():
-    keyboard.write(enter)
-    keyboard.write(confreg + register1 + enter)
-    time.sleep(3.5)
-    keyboard.write(reset + enter)
+    def asa_file_del(self):
 
-def wap_rommon_exec():
-    keyboard.write(enter)
-    keyboard.write(rm + 'private_multiple_fs' + enter + 'y' + enter)
-    time.sleep(0.1)
-    keyboard.write(reset + enter + 'y' + enter)
+        # ASA Deletion Logic
+        self.write(enter)
+        for w in alist:
+            self.write(rm1 + w + 4 * enter)
+            time.sleep(sleep_time)
+        self.write('# Succesfully Removed/Attempted to Remove ' + alen + ' files from ASA!\n# Here is a list of remaining files: \n' + dirflash)
 
-def switch_rommon_exec():
-    keyboard.write(enter)
-    for y in slist:
-        keyboard.write(rm + y + enter + 'y' + enter)
-        time.sleep(sleep_time)
+class rommon_del(main_del):
 
-def asa_rommon_exec():
-    keyboard.write('escape')
-    keyboard.write(confreg + space + register1)
-    time.sleep(10)
-    keyboard.write('boot' + enter)
+    # ROMMON RESET Functions
 
-"""
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--window_size=1920X1080")
+    def router_rommon_exec(self):
+        self.write(enter)
+        self.write(confreg + register1 + enter)
+        time.sleep(3.5)
+        self.write(reset + enter)
+
+    def wap_rommon_exec(self):
+        self.write(enter)
+        self.write(rm + 'private_multiple_fs' + enter + 'y' + enter)
+        time.sleep(0.1)
+        self.write(reset + enter + 'y' + enter)
+
+    def switch_rommon_exec(self):
+        self.write(enter)
+        for y in slist:
+            self.write(rm + y + enter + 'y' + enter)
+            time.sleep(sleep_time)
+
+    def asa_rommon_exec(self):
+        self.write('escape')
+        self.write(confreg + space + register1)
+        time.sleep(10)
+        self.write('boot' + enter)
+
+
+class Tech_stuff():
+    
+    import time
+    from selenium.webdriver.chrome.options import Options
+    from selenium import webdriver
+
+    def __init__(self):
+        self.options = Options()
+        # options.add_argument("--headless")
+        self.options.add_argument("--window_size=1920X1080")
         
-driver = webdriver.Chrome(chrome_options=options, executable_path=chromedriver)
-driver.get(link)
+        self.driver = webdriver.Chrome(chrome_options=self.options, executable_path=chromedriver)
+        self.driver.get(link)
 
-def print_basic_tech(asset, palletinput, gradeinput, processinput, complianceinput):
-    driver.find_element_by_xpath(asset_input)
-    driver.send_keys(asset + enter)
-    driver.find_element_by_xpath(pallet)
-    driver.send_keys(palletinput)
-    driver.find_element_by_xpath(grade)
-    driver.send_keys(gradeinput)
-    driver.find_element_by_xpath(next_process)
-    driver.send_keys(processinput)
-    driver.find_element_by_xpath(compliance_label)
-    driver.send_keys(complianceinput)
+    def find_element(self, xpath, data):
+        self.findthing = self.driver.find_element_by_xpath(xpath)
+        self.findthing.send_keys(data)
+        time.sleep(0.25)
+    
+    def click_element(self, xpath2):
+        self.click = self.driver.find_element_by_xpath(xpath2)
+        self.click.click()
 
-def new_misc()
-        driver.find_element_by_xpath(misc)
-        driver.click(misc)
+    def new_misc(self):
+        self.click_element(misc)
 
-def print_misc_info(license, random, custom)
-    new_misc()
-    driver.find_element_by_xpath(misc_textbx)
-    driver.send_keys(license)
-    new_misc()
-    driver.find_element_by_xpath(misc_textbx_2)
-    driver.send_keys(random)
-    new_misc()
-    driver.find_element_by_xpath(misc_textbx_3)
-    driver.send_keys(custom)
+    def print_basic_tech(self, asset, palletinput, gradeinput, processinput, complianceinput):
+        self.find_element(asset_input, asset)
+        self.time.sleep(1)
+        map(self.find_element, (pallet, grade, next_process, compliance_label), (palletinput, gradeinput, processinput, complianceinput))
 
-def submit_tech()
-    driver.find_element_by_xpath(submit_asset)
-"""
+    def print_misc_info(self, license, random, custom):
+        self.new_misc()
+        self.find_element(misc_textbx, license)
+        self.new_misc()
+        self.find_element(misc_textbx_2, random)
+        self.new_misc()
+        self.find_element(misc_textbx_3, custom)
+
+    def submit_tech(self):
+        self.click_element(submit_asset)
+
+class serialwords():
+
+    def __init__(self):
+        pass
+
+    def options(self):
+        pass
+
+class ser_open(Tech_stuff):
+    
+    import serial
+    import keyword
+
+    def __init__(self, port, baud):
+        self.ser = serial.Serial(port=port, baudrate=baud)
+        self.ser.open()
+        self.is_reading_done = False
+        self.is_wap = False
+        self.is_switch = False
+        self.is_router = False
+        self.is_asa = False
+        self.is_in_rommon = False
+        self.is_in_priv_exec = False
+    
+    def reader(self):
+        # In theory, read 5 lines, place them into a list, and update _is reading done to trigger the word search function.
+        num = 0 
+        self.lines = []
+        while num < 1:
+            self.ser.read_until().append(self.lines)
+            num = num + 1
+        self.is_reading_done = True
+    
+    def search_keywords(self):
+        if self.is_reading_done == True:
+            words_and_phrases = ['#', ":", 
+#2
+        "Would you like to enter the initial configuration dialog? [yes/no]", 
+        "Press RETURN to get started!", 
+        "Router#", 
+        "ap:", 
+        "examining image...", 
+#7        
+        "Restricted Rights Legend", 
+        "Username:", 
+        "User Access Verification", 
+        "Password:", 
+        "capwap process not yet started.Please execute enable command again", 
+#12        
+        "% Invalid input detected at '^' marker.", 
+        "con0 is now available",
+        "Use BREAK or ESC to interrupt boot.", 
+        "rommon #>", 
+        "Pre-configure Firewall now through interactive prompts [yes]?", 
+#17
+        "Licensed features for this platform:", 
+        
+        ]
+            if(words_and_phrases[2] in self.lines):
+                
+                self.no_config_dialogue()
+                self.lines = []
+            
+            elif(words_and_phrases[8] or words_and_phrases[10] in self.lines):
+                
+                self.username_and_pass()
+                self.lines = []
+            
+            elif(words_and_phrases[3] or words_and_phrases[13] in self.lines):
+                
+                self.ser.write("\n")
+                self.lines = []
+            
+            elif(words_and_phrases[4] in self.lines):
+                
+                self.is_router = True
+            
+            elif(words_and_phrases[5] in self.lines):
+
+                self.is_wap = True
+            
+            elif(words_and_phrases[14] in self.lines):
+
+                self.is_asa = True
+
+            elif(words_and_phrases[15] in self.lines):
+
+                self.is_in_rommon = True
+            
+            elif(words_and_phrases[16] in self.lines):
+
+                self.is_asa = True
+
+            elif(words_and_phrases[17] in self.lines):
+                
+                self.info = True
+            
+            else:
+                self.lines = []
+            
+    def no_config_dialogue(self):
+        no = 'no'
+        self.ser.write(no.encode())
+        self.ser.write('\n')
+
+    def username_and_pass(self):
+        cisco = 'Cisco'
+        self.ser.write(cisco.encode())
+        self.ser.write('\n')
 
 # Toggling the while loop for break loop
 
@@ -201,29 +330,18 @@ while True:
         notstatus = 'ON'
 """
 
-input('What do you want to do? Type "help" for help\n>>')
+main_del = main_del()
 
-# Initial loops for searching for keypresses
+with keyboard.GlobalHotKeys({
 
-one = 1
-while True:
-    if keyboard.is_pressed('F5'):
-        wap_file_del()
-        print(wlen)
+'<f5>': main_del.wap_file_del(), 
+'<f6>': main_del.switch_file_del(), 
+'<f7>': main_del.router_file_del(), 
+'<f8>': main_del.asa_file_del()
 
-    elif keyboard.is_pressed('F6'):
-        switch_file_del()
-        print(slen)
+}) as hkeys:
+    hkeys.join()
 
-    elif keyboard.is_pressed('F7'):
-        router_file_del()
-        print(rlen)
-    
-    elif keyboard.is_pressed('F8'):
-        asa_file_del()
-        print(alen)
-    
-    time.sleep(0.1)
-
+print("HI")
 # Loops for looking for command input
 
