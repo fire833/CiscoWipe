12d5de087772123886624532862035fc45f255f9
fire833 <kttpsy@gmail.com>
2021-04-06 15:17:09 -0500

    idk what this is

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 
 import re
 import os
 
 import tensorflow as tf
 import pandas as pd
-
-=======
-
-import re
-import os
-
-import tensorflow as tf
-import pandas as pd
-
->>>>>>> 702a546da1e7687c7f6840999a5263ae5a294833
